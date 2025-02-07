387c72c6b4f4b742cb0f921ec432134dd7735256
Ami Halsey <cro19030@byui.edu>
2025-02-07 00:35:02 -0700

    finished conditions.html

--- src/js/pages/conditions.mjs
+++ src/js/pages/conditions.mjs
@@ -1,13 +1,48 @@
-import { getParkData } from "../parkService.mjs";
+import { getAlertData, getParkData , getVisitorCenterData , getActivitiesData} from "../parkService.mjs";
 import { setHeaderInfo } from "../setHeaderFooter.mjs";
-import { setParkFooter } from "../templates.mjs";
+import { setParkFooter , alertCardTemplate , visitorCardTemplate , activitesCardTemplate} from "../templates.mjs";
 
+function setParkAlertLinks(alerts) {
+    const alertContainer = document.querySelector("#alerts");
+    alerts.forEach(alertInfo => {
+        alertContainer.innerHTML += alertCardTemplate(alertInfo);
+    });
+};
 
+function setVisitorCentersLink(data) {
+    const detailsElement = document.querySelector("#visitorServices details[name='current-details']")
+    // const dataContainer = document.querySelector("#visitorServices");
+
+    detailsElement.addEventListener("toggle", function () {
+        if (detailsElement.open) {
+            data.forEach(centerInfo => {
+                detailsElement.innerHTML += visitorCardTemplate(centerInfo);
+            })
+        }
+    })
+}
+
+function setActivitiesLink(data) {
+    const detailsElement = document.querySelector("#activities details[name='current-details']")
+    detailsElement.addEventListener("toggle", function () {
+        if (detailsElement.open) {
+            data.forEach(activityInfo => {
+                detailsElement.innerHTML += activitesCardTemplate(activityInfo);
+            })
+        }
+    })
+}
 
 (async function init() {
     const parkData = await getParkData();
+    const alertData = await getAlertData();
+    const centerData = await getVisitorCenterData();
+    const activityData = await getActivitiesData();
 
     setHeaderInfo(parkData);
+    setParkAlertLinks(alertData);
+    setVisitorCentersLink(centerData);
+    setActivitiesLink(activityData);
 
     const footer = document.querySelector('#park-footer');
     footer.innerHTML = setParkFooter(parkData);
--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -1,4 +1,4 @@
-import { getParkData , getInfoLinks} from "./parkService.mjs";
+import { getAlertData, getParkData , getInfoLinks} from "./parkService.mjs";
 import { getVoicePhone, getMailingAddress } from "./setHeaderFooter.mjs";
 
 export function parkInfoTemplate(data) {
@@ -29,6 +29,47 @@ export function mediaCardTemplate(info) {
         <p>${info.description}</p>
     </div>
     `;
+}
+
+export function alertCardTemplate(alert) {
+    let alertType = "";
+    switch (alert.category) {
+        case "Park Closure":
+            alertType = "closure";
+            break;
+        default:
+            alertType = alert.category.toLowerCase();
+    }
+    return `<li class="alert">
+    <svg class="icon" role="presentation" focusable="false">
+        <use
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        xlink:href="/images/sprite.symbol.svg#alert-${alertType}"
+        ></use>
+    </svg>
+    <div>
+        <h3 class="alert-${alertType}">${alert.title}</h3>
+        <p>${alert.description}</p>
+    </div></li>
+    `;
+}
+
+export function visitorCardTemplate(data) {
+    return `
+    <div class="visitorCenters">
+        <h3>${data.name}</h3>
+        <p>${data.description}</p>
+        <p>${data.directionsInfo}</p>
+    </div>
+    `
+}
+
+export function activitesCardTemplate(data) {
+    return `
+    <ul>
+        <li>${data.name}</li>
+    </ul>
+    `
 }
 
 export function setParkFooter(info) {
